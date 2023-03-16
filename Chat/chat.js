ac7380720ecf83af5538535862cbe67d9df72131
Prashanth-Reddy19 <kotha.prashanth02>
2023-03-16 19:09:49 +0530

    optimize the chat

--- Chat/chat.js
+++ Chat/chat.js
@@ -8,7 +8,7 @@ sendbtn.addEventListener('click', sendmessage);
 
 async function sendmessage(e) {
     try {
-        e.preventDefault();
+        
         if (message.value === '') {
             msg.innerHTML = "Please Enter message";
             setTimeout(() => {
@@ -39,8 +39,30 @@ async function sendmessage(e) {
     }
 }
 
+//decode token
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
 async function showmessage(name, message) {
     try {
+        const token=localStorage.getItem('token');
+        const decodedtoken=parseJwt(token);
+        let className;
+        if(name===decodedtoken.name)
+        {
+          className='currentuser'
+        }
+        else{
+            className-'otheruser'
+        }
+        chattable.className="tbody";
         chattable.innerHTML += `<tr><td>${name}-${message}</td></tr>`;
     } catch (err) {
         console.log(err);
@@ -51,14 +73,38 @@ window.addEventListener('DOMContentLoaded', getchats);
 
 async function getchats(e) {
     try {
-        e.preventDefault();
+        
+        let lastmsgId=JSON.parse(localStorage.getItem('lastmsgId'));
+        console.log(lastmsgId,">>>>>>>>>>>>")
 
-        const response = await axios.get('http://localhost:3000/chat/get-message')
+        const response = await axios.get(`http://localhost:3000/chat/get-message/${lastmsgId}`)
 
         console.log(response, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        lastmsgId+=parseInt(response.data.message.length);
+        console.log(lastmsgId,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-        for (let i = 0; i < response.data.message.length; i++)
-            showmessage(response.data.message[i].name, response.data.message[i].message)
+        let existingArray=JSON.parse(localStorage.getItem('messages'))||[];
+
+        if(existingArray.length>=10){
+         existingArray=[];
+        }
+
+        let responseArray=response.data.message;
+
+        let mergedArray=existingArray.concat(responseArray);
+
+        localStorage.setItem('lastmsgId',JSON.stringify(lastmsgId));
+
+        localStorage.setItem('messages',JSON.stringify(mergedArray));
+
+        chattable.innerHTML='';
+
+        for(let i=0;i<mergedArray.length;i++){
+             showmessage(mergedArray[i].username,mergedArray[i].message)
+        }
+        chattable.scrollTop=chattable.scrollHeight;
+
+       
 
     }
     catch (err) {
